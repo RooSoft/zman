17baccdf4a91332083008700fa7123c3dc4a1e13
Marc Lacoursiere <marc@roosoft.com>
2019-10-30 15:11:18 -0400

    match ZFS snapshots according to zman.yaml configuration

--- index.mjs
+++ index.mjs
@@ -1,5 +1,18 @@
 import { readConfig } from './lib/yaml.mjs'
+import { getSnapshots } from './lib/zfs/index.mjs'
 
-const pools = readConfig('./zman.yaml')
+const zmanConfig = readConfig('./zman.yaml')
+let snapshots = {}
 
-console.dir(pools, {depth: null})
+zmanConfig.pools.forEach(poolConfig => {
+  const poolSnapshots = snapshots[poolConfig.name] = {}
+
+  poolConfig.frequencies.forEach(frequency => {
+    const frequencySnapshots = poolSnapshots[frequency.type] = []
+    const snapshot = getSnapshots(poolConfig.name, frequency.type)
+
+    frequencySnapshots.push(snapshot)
+  })
+})
+
+console.dir(snapshots, { depth: null })
