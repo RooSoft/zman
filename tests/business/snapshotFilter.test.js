af1a5f6ee6f76842b4f44ef4a8e93bd88340c849
Marc Lacoursiere <marc@roosoft.com>
2019-11-01 11:04:32 -0400

    test expired snapshots

--- tests/business/snapshotFilter.test.js
+++ tests/business/snapshotFilter.test.js
@@ -32,3 +32,31 @@ test('Should sort out snapshots by pools and frequencies according to yaml confi
   expect(poolSnapshots['largepool/whatever']['monthly']).toHaveLength(5)
   expect(poolSnapshots['smallpool/zman']['monthly']).toHaveLength(4)
 })
+
+test('Should sort out snapshots by pools and frequencies according to yaml config file', () => {
+  const zmanConfig = readConfig('../../zman.yaml')
+
+  const now = new Date('2019-11-1')
+
+  const snapshots = parseSnapshots(DUMMY_SNAPSHOT_OUTPUT)
+  const snapshotsByPool = sortSnapshotsByPool(snapshots)
+  const poolSnapshots = getRelatedSnapshots(zmanConfig, snapshotsByPool)
+  const expiredSnapshots = getExpiredSnapshots(now, zmanConfig, poolSnapshots)
+
+  expect(expiredSnapshots).toHaveLength(7)
+
+  const expiredSnapshotNames = [
+    'largepool/whatever@zman-hourly-2019-10-29-14:14',
+    'largepool/whatever@zman-daily-2019-09-27-14:14',
+    'largepool/whatever@zman-daily-2019-09-28-14:14',
+    'largepool/whatever@zman-monthly-2019-06-28-14:14',
+    'largepool/whatever@zman-monthly-2019-07-28-14:14',
+    'largepool/whatever@zman-monthly-2019-08-28-14:14',
+    'smallpool/zman@zman-monthly-2019-07-29-14:14',
+  ]
+
+  expiredSnapshotNames.map(expiredSnapshotName => {
+    const example = expiredSnapshots.filter(snapshot => snapshot.name === expiredSnapshotName)
+    expect(example).toHaveLength(1)
+  })
+})
