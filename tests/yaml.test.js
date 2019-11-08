3c02dd5bf67e97be86a351fadfd8042102be7d3f
Marc Lacoursiere <marc@roosoft.com>
2019-11-08 13:03:50 -0500

    test a pool without any frequencies in config file

--- tests/yaml.test.js
+++ tests/yaml.test.js
@@ -5,14 +5,14 @@ test('Should be able to read config file', () => {
   const zmanConfig = readConfig('tests/config/zman.yaml')
 
   expect(zmanConfig).toHaveProperty('pools')
-  expect(zmanConfig.pools).toHaveLength(2)
+  expect(zmanConfig.pools).toHaveLength(3)
 
   const smallpool = zmanConfig.pools[0]
 
   expect(smallpool.name).toBe('smallpool/zman')
   expect(smallpool.frequencies).toHaveLength(3)
 
-  const largepool = zmanConfig.pools[1]
+  const largepool = zmanConfig.pools[2]
 
   expect(largepool.name).toBe('largepool/whatever')
   expect(largepool.frequencies).toHaveLength(3)
@@ -20,7 +20,7 @@ test('Should be able to read config file', () => {
 
 test('Should fail to load nonexisting config file', () => {
   const configFile = 'tests/config/doNotExist.yaml'
-
+console.log('hellloooooooo')
   expect(() => {
     readConfig(configFile)
   }).toThrow(new ConfigError(`Config file not found: ${configFile}`))
